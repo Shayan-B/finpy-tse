c99810a66e4e3395f1025edf76aefdc62b392044
Shayan Bastani <32581929+Shayan-B@users.noreply.github.com>
2023-06-28 23:51:40 +0330

    changed init file import method

--- finpy_tse/__init__.py
+++ finpy_tse/__init__.py
@@ -1,33 +1,8 @@
-# imports:
-import pandas as pd
-import numpy as np
-
-import requests
-from bs4 import BeautifulSoup
-import urllib3
-urllib3.disable_warnings()
-
-import aiohttp
-import asyncio
-from unsync import unsync
-import tracemalloc
-
-import datetime
-import jdatetime
-import calendar
-import time
-import re
-
-from persiantools import characters
-from IPython.display import clear_output
-
-from . import(
-    indices,
-    intraday,
-    market,
-    misc,
-    price,
-    queue,
-    shareholders,
-    util
-)
+from .indices import *
+from .intraday import *
+from .market import *
+from .misc import *
+from .price import *
+from .queue import *
+from .shareholders import *
+from .util import *
